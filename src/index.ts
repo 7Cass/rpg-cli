ed3e17c3dd576311d605e4031bbcbb31520c44b6
Joao Pedro <jaaopbr@gmail.com>
2023-09-05 13:01:21 -0300

    feat: initial game loop

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,56 @@
-import http from 'http';
+import * as readline from 'readline';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
 
-export const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    JSON.stringify({
-      data: 'It Works!',
-    })
-  );
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+function question(prompt: string): Promise<string> {
+  return new Promise(resolve => rl.question(prompt, resolve));
+}
+
+async function loop(): Promise<void> {
+  while (true) {
+    console.log(chalk.bold.cyan('\n--- MENU PRINCIPAL ---'));
+    console.log('1. ' + chalk.yellow('👀 - Explorar'));
+    console.log('2. ' + chalk.yellow('🎒 - Ver inventário'));
+    console.log('3. ' + chalk.yellow('📊 - Ver status'));
+    console.log('4. ' + chalk.green('🚪 - Sair'));
+
+    const { escolha } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'escolha',
+        message: 'O que você gostaria de fazer?',
+        choices: ['Explorar', 'Ver inventário', 'Ver status', 'Sair'],
+      },
+    ]);
+
+    switch (escolha) {
+      case 'Explorar':
+        console.log(chalk.yellow('Você decide explorar a área ao redor...'));
+        // Lógica de exploração
+        break;
+      case 'Ver inventário':
+        console.log(chalk.yellow('Você verifica seus pertences...'));
+        // Lógica do inventário
+        break;
+      case 'Ver status':
+        console.log(chalk.yellow('Você confere sua condição atual...'));
+        // Lógica para ver status
+        break;
+      case 'Sair':
+        console.log(chalk.green('Até logo!'));
+        rl.close();
+        process.exit(0);
+        break;
+      default:
+        console.log(chalk.red('Opção inválida!'));
+    }
+  }
+}
+
+console.log(chalk.bold.green('Bem-vindo ao RPG Textual!'));
+loop();
